78ec31ab1566a0fa35688931d468dea7db27ce3b
thaohvm <43254779+thaohvm@users.noreply.github.com>
2020-10-02 22:50:40 -0700

    Added Listings model

--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,37 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return f"{self.username} - {self.email}"
+
+
+class Listings(models.Model):
+    item = models.CharField(max_length=64)
+    price = models.FloatField()
+    currency = models.CharField(max_length=3)
+    image = models.ImageField(upload_to="uploads/images", blank=True)
+    description = models.CharField(max_length=64, blank=True)
+    created = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (
+            f"{self.item} : {self.description} - "
+            f"{self.price}{self.currency} - {self.created}"
+        )
+
+# class Bids(models.Model):
+#     item = models.CharField(max_length=64)
+#     bid_th = models.IntegerField()
+#     bid = models.IntegerField()
+
+#     def __str__(self):
+#         return f"{self.item} : {self.bid_th} ({self.bid})"
+
+
+# class Comments(models.Model):
+#     user = models.CharField(max_length=64)
+#     content = models.IntegerField()
+
+#     def __str__(self):
+#         return f"{self.item} : {self.bid_th} ({self.bid})"
